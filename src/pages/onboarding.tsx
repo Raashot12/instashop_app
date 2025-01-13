864c79752dc13aa631c103f78ec9f511d5fb75e4
Rasheed Iskilu <riskilu@plateaumed.com>
2025-01-13 14:41:59 +0100

    WIP

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -2,6 +2,7 @@ import CreateProduct from "@/components/CreateProduct"
 import CreateStoreForm from "@/components/CreateStoreForm"
 import {useAppSelector} from "@/components/hooks"
 import PhoneEmailScreen from "@/components/PhoneEmail"
+import ProductDetails from "@/components/ProductDetails"
 import ProfileSetupScreen from "@/components/ProfileSetup"
 import {selectOnboardingSteps} from "@/state/onboardingSlice"
 import React from "react"
@@ -11,10 +12,11 @@ const OnboardingPage = () => {
 
   return (
     <div>
-      {/* <>{onboardingObject?.currentStep === 1 && <PhoneEmailScreen />}</> */}
+      <>{onboardingObject?.currentStep === 1 && <PhoneEmailScreen />}</>
       <>{onboardingObject?.currentStep === 2 && <ProfileSetupScreen />}</>
       <>{onboardingObject?.currentStep === 3 && <CreateStoreForm />}</>
-      <>{onboardingObject?.currentStep === 1 && <CreateProduct />}</>
+      <>{onboardingObject?.currentStep === 4 && <CreateProduct />}</>
+      <>{onboardingObject?.currentStep === 5 && <ProductDetails />}</>
     </div>
   )
 }
